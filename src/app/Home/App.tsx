f9fdb16c39813dc6fc2f2d4457f3cb61630e61a9
Carlos Eduardo <kadufenix10@gmail.com>
2025-09-22 18:57:25 -0300

    feat: implement item addition functionality and improve filter component

--- src/app/Home/App.tsx
+++ src/app/Home/App.tsx
@@ -1,4 +1,4 @@
-import { FlatList, Image, Text, TouchableOpacity, View } from "react-native";
+import { FlatList, Image, Text, TouchableOpacity, View, Alert } from "react-native";
 
 import { Button } from "@/components/Button";
 import { Filter } from "@/components/Filter";
@@ -6,41 +6,49 @@ import { Input } from "@/components/Input";
 import { Item } from "@/components/Item";
 import { FilterStatus } from "@/types/FilterStatus";
 import { styles } from "./styles";
+import { useState } from "react";
 
 const FILTER_STATUS: FilterStatus[] = [FilterStatus.PENDING, FilterStatus.DONE];
 
-const ITEMS = [
-  {
-    id: "1",
-    status: FilterStatus.DONE,
-    description: "1 pacote de café",
-  },
-  {
-    id: "2",
-    status: FilterStatus.PENDING,
-    description: "3 pacotes de macarrão",
-  },
-  {
-    id: "3",
-    status: FilterStatus.PENDING,
-    description: "3 cebolas",
-  },
-];
+type ItemType = {
+  id: string;
+  description: string;
+  status: FilterStatus;
+};
 
 export function Home() {
+  const [filter, setFilter] = useState<FilterStatus>(FilterStatus.PENDING);
+  const [description, setDescription] = useState<string>("");
+  const [items, setItems] = useState<ItemType[]>([]);
+
+  function handleAdd() {
+    if (!description.trim()) {
+      return Alert.alert("Adicionar", "Informe a descrição para adicionar um item.");
+    }
+
+    const newItem = {
+      id: Math.random().toString(36).substring(2),
+      description: description,
+      status: FilterStatus.PENDING,
+    };
+
+    setItems((prevItems) => [...prevItems, newItem]);
+    setDescription("");
+  }
+
   return (
     <View style={styles.container}>
       <Image source={require("@/assets/logo.png")} style={styles.logo} />
 
       <View style={styles.form}>
-        <Input placeholder="O que você precisa comprar?" />
-        <Button title="Entrar" />
+        <Input placeholder="O que você precisa comprar?" value={description} onChangeText={setDescription} />
+        <Button title="Adicionar" onPress={handleAdd} />
       </View>
 
       <View style={styles.content}>
         <View style={styles.header}>
           {FILTER_STATUS.map((status) => (
-            <Filter key={status} status={status} isActive />
+            <Filter key={status} status={status} isActive={filter === status} onPress={() => setFilter(status)} />
           ))}
 
           <TouchableOpacity style={styles.clearButton}>
@@ -49,15 +57,9 @@ export function Home() {
         </View>
 
         <FlatList
-          data={ITEMS}
+          data={items}
           keyExtractor={(item) => item.id}
-          renderItem={({ item }) => (
-            <Item
-              data={{ status: item.status, description: item.description }}
-              onRemove={() => {}}
-              onStatus={() => {}}
-            />
-          )}
+          renderItem={({ item }) => <Item data={item} onRemove={() => {}} onStatus={() => {}} />}
           showsVerticalScrollIndicator={false}
           ItemSeparatorComponent={() => <View style={styles.separator} />}
           contentContainerStyle={styles.listContent}
--- src/components/Filter/index.tsx
+++ src/components/Filter/index.tsx
@@ -10,11 +10,7 @@ interface FilterProps extends TouchableOpacityProps {
 
 export function Filter({ status, isActive, ...rest }: FilterProps) {
   return (
-    <TouchableOpacity
-      style={[styles.container, { opacity: isActive ? 1 : 0.5 }]}
-      activeOpacity={0.8}
-      {...rest}
-    >
+    <TouchableOpacity style={[styles.container, { opacity: isActive ? 1 : 0.5 }]} activeOpacity={0.8} {...rest}>
       <StatusIcon status={status} />
 
       <Text style={styles.title}>{status === FilterStatus.DONE ? "Comprados" : "Pendentes"}</Text>
